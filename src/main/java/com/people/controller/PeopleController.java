7d33d625a35bc06907dbd04144488b3e5a274dde
Enoque Felipe <enoquefelipe@gmail.com>
2018-11-26 10:44:43 -0200

    Method createURI created

--- src/main/java/com/people/controller/PeopleController.java
+++ src/main/java/com/people/controller/PeopleController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,15 +27,20 @@ public class PeopleController {
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody Person person, UriComponentsBuilder uriBuilder){
-        URI location = uriBuilder.path("/api/person/{id}").buildAndExpand(service.save(person).getId()).toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(location);
+        HttpHeaders headers = createURI(uriBuilder, service.save(person));
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "person/{id}")
     public Optional<Person> getPerson(@PathVariable String id) {
         return service.findById(id);
+    }
+
+    private HttpHeaders createURI(UriComponentsBuilder uriBuilder, Person person) {
+        URI location = uriBuilder.path("/api/person/{id}").buildAndExpand(person.getId()).toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return headers;
     }
 
 }
