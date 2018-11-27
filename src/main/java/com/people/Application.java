97e0bc2105ac5906be47ba1801b93d0eb2881a97
Enoque Felipe <enoquefelipe@gmail.com>
2018-11-27 19:47:10 -0200

    First commit

--- src/main/java/com/people/Application.java
+++ src/main/java/com/people/Application.java
@@ -2,10 +2,6 @@ package com.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/com/people/controller/PeopleController.java
+++ src/main/java/com/people/controller/PeopleController.java
@@ -1,46 +1,72 @@
 package com.people.controller;
 
-import com.people.model.Person;
-import com.people.service.PeopleService;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
+import com.people.model.Person;
+import com.people.model.Status;
+import com.people.service.PeopleService;
 
 @RestController
 @RequestMapping("/api")
 public class PeopleController {
 
-    @Autowired
-    private PeopleService service;
+	@Autowired
+	private PeopleService service;
 
-    @GetMapping("/people")
-    public @ResponseBody List<Person> getAll(){
-        return (List<Person>) service.findAll();
-    }
+	@GetMapping("/people")
+	public @ResponseBody List<Person> getAllPerson() {
+		return (List<Person>) service.findAll();
+	}
 
-    @PostMapping("/register")
-    public ResponseEntity register(@RequestBody Person person, UriComponentsBuilder uriBuilder){
-        HttpHeaders headers = createURI(uriBuilder, service.save(person));
-        return new ResponseEntity<>(headers, HttpStatus.CREATED);
-    }
+	@PostMapping("/register")
+	public ResponseEntity<?> registerPerson(@RequestBody Person person, UriComponentsBuilder uriBuilder) {
+		HttpHeaders headers = createURI(uriBuilder, service.save(person));
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
+	}
 
-    @GetMapping(value = "person/{id}")
-    public Optional<Person> getPerson(@PathVariable String id) {
-        return service.findById(id);
-    }
+	@GetMapping(value = "person/{id}")
+	public ResponseEntity<?> findPerson(@PathVariable String id) {
+		Optional<Person> person = service.findById(id);
+		return Optional.empty().equals(person) ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(person, HttpStatus.OK);
+	}
 
-    private HttpHeaders createURI(UriComponentsBuilder uriBuilder, Person person) {
-        URI location = uriBuilder.path("/api/person/{id}").buildAndExpand(person.getId()).toUri();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(location);
-        return headers;
-    }
+	@DeleteMapping(value = "person/{id}")
+	public ResponseEntity<?> removePerson(@PathVariable String id) {
+
+		if (!existPerson(id)) {
+			service.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+
+	}
+
+	private HttpHeaders createURI(UriComponentsBuilder uriBuilder, Person person) {
+		URI location = uriBuilder.path("/api/person/{id}").buildAndExpand(person.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+		return headers;
+	}
+
+	private boolean existPerson(String id) {
+		return Optional.empty().equals(service.findById(id));
+	}
 
 }
